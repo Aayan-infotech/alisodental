e091a1fb327b0628379c921764e29ea46432efac
Rishabh Chandra <rishabh.chandra@aayaninfotech.com>
2025-09-17 17:29:46 +0530

    new changes

--- wp-config.php
+++ wp-config.php
@@ -91,6 +91,9 @@ define('WP_DEBUG', false);
 /* Add any custom values between this line and the "stop editing" line. */
 
 
+
+/* That's all, stop editing! Happy publishing. */
+
 /** Absolute path to the WordPress directory. */
 if (! defined('ABSPATH')) {
 	define('ABSPATH', __DIR__ . '/');
